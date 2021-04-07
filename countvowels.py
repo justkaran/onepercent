ce7741a0208e055d0161664d9ebe178ed74efc7a
Thomas <harrypeppermint@gmail.com>
2021-04-07 23:44:38 +0100

    Small optimizations

--- countvowels.py
+++ countvowels.py
@@ -1,13 +1,13 @@
 # Create a program that finds out how many times the letter ‘e’ is said in the text and then displays it as a percentage of all the letters.
 
-string = "hello"
+test_string = "hello"
 
-
-def countvowels(string):
-    num_vowels=0
+def count_vowels(string):
+    num_vowels = 0
+    string = string.lower()
     for char in string:
-        if char in "aeiouAEIOU":
-           num_vowels = num_vowels+1
+        if char in "aeiou":
+           num_vowels += 1
     return num_vowels
 
-print(countvowels(string))
+print(count_vowels(test_string))
